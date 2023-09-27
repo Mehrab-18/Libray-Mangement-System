e660cd1a273a9834580ae6e59a75ebda55a879d2
mehrab1802 <mehrabmehmood18@gmail.com>
2023-09-27 18:06:56 +0500

    added border to button

--- components/PrivacyButton.tsx
+++ components/PrivacyButton.tsx
@@ -2,7 +2,7 @@ import { AiOutlineLock } from "react-icons/ai";
 
 const PrivacyButton = () => {
   return (
-    <button className="fixed top-1/2 left-0 transform -translate-y-1/2 flex flex-row items-center gap-2 bg-white text-black py-2 px-4 rounded-3xl">
+    <button className="fixed top-[70%] z-20 left-0 border ml-2 border-custom-mobile-orange transform -translate-y-1/2 flex flex-row items-center gap-2 bg-white text-black py-2 px-4 rounded-3xl">
       <span className="text-red-400">
         <AiOutlineLock />
       </span>{" "}
